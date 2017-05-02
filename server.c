e876fbfa00dd2d7e507a6873ef70da0a6808ea10
Adrian Preston <prestaj0@sewanee.edu>
2017-05-02 14:39:09 -0500

    don't remember might be literally one comment

--- server.c
+++ server.c
@@ -1,8 +1,15 @@
-/* $begin tinymain */
 /*
- * tiny.c - A simple, iterative HTTP/1.0 Web server that uses the 
- *     GET method to serve static and dynamic content.
- */
+*
+*    server.c - An iterative proxy server for cs428 Final Project
+*    By:
+*       Adrian Preston       prestaj0@sewanee.edu
+*       Daniel Evans         evansdb0@sewanee.edu
+*       Blaise Iradukunda    iradub0sewanee.edu
+*
+*    The University of the South - Easter Semester 2017
+*
+*/
+
 #include "server.h"
 
 int num_requests;
